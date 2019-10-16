93655d19375393068d16327aab8581b53d99641f
Christian Cavasin <christian.cavasin@9elements.com>
2019-10-16 18:55:02 +0200

    Add embed_migrations

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -7,6 +7,8 @@ extern crate rocket_contrib;
 #[macro_use]
 extern crate diesel;
 #[macro_use]
+extern crate diesel_migrations;
+#[macro_use]
 extern crate serde;
 
 use rocket_contrib::json::Json;
@@ -18,12 +20,14 @@ use rocket::fairing::AdHoc;
 use rocket::request::{Form, FlashMessage};
 use rocket::response::{Flash, Redirect};
 
+embed_migrations!();
+
 #[database("sqlite")]
 struct Database(SqliteConnection);
 
 mod schema;
 
-use schema::{users, users::dsl::users as all_users};
+use schema::users::{self, dsl};
 
 #[table_name = "users"]
 #[derive(Serialize, Queryable, Insertable, Debug, Clone)]
@@ -35,16 +39,25 @@ pub struct User {
 
 #[get("/users/<id>")]
 fn user(database: Database, id: i32) -> Option<Json<User>> {
-    if let Ok(user) = all_users.find(id).get_result::<User>(&database.0) {
+    if let Ok(user) = dsl::users.find(id).get_result::<User>(&database.0) {
         Some(Json(user))
     } else {
         None
     }
 }
 
+fn run_db_migrations(rocket: Rocket) -> Result<Rocket, Rocket> {
+	let database = Database::get_one(&rocket).expect("database connection");
+	match embedded_migrations::run(&*database) {
+		Ok(()) => Ok(rocket),
+		Err(e) => Err(rocket)
+	}
+}
+
 fn main() {
     rocket::ignite()
         .attach(Database::fairing())
+		.attach(AdHoc::on_attach("Database Migrations", run_db_migrations))
         .mount("/", routes![user])
         .launch();
 }
